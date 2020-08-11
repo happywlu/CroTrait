0b1f447c3c52111829fbc69dc9c38174e7d79ffa
happywlu <wlubio@sina.com>
2020-08-11 23:24:10 +0800

    new_type

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Lenovo1
+# @Date:   2020-08-11 23:20:52
+# @Last Modified by:   Lenovo1
+# @Last Modified time: 2020-08-11 23:21:12
+
+
+import os
+
+print(os.getcwd())
