a374d8ac50785598dda50784699ae6161c32bb63
happywlu <wlubio@sina.com>
2020-08-12 13:56:49 +0800

    script

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
-# @Author: Lenovo1
+# @Author: Wang Lu
 # @Date:   2020-08-11 23:20:52
 # @Last Modified by:   Lenovo1
-# @Last Modified time: 2020-08-11 23:21:12
+# @Last Modified time: 2020-08-12 13:56:15
 
 
-import os
+from os import getcwd
 
-print(os.getcwd())
+from os import listdir
+
+wf = open("test.fasta", "w")
+with open("Cronobacter_OACs.fasta") as f:
+	for line in f:
+		if ">" in line and "galF" in line:
+			m = line.strip("\n").split("_")[1]
+			wf.write(">"+m+"\n")
+			wf.write(next(f))
+
+wf.close()
