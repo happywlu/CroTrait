8e042bf826e6a52c29d59f0673a19572e97b729d
happywlu <wlubio@sina.com>
2020-08-12 14:40:43 +0800

    test

--- test.py
+++ test.py
@@ -2,19 +2,37 @@
 # @Author: Wang Lu
 # @Date:   2020-08-11 23:20:52
 # @Last Modified by:   Lenovo1
-# @Last Modified time: 2020-08-12 13:56:15
+# @Last Modified time: 2020-08-12 14:38:56
 
 
-from os import getcwd
+import os
+import datetime
+import argparse
 
-from os import listdir
+today=datetime.date.today().strftime('%Y%m%d')
 
-wf = open("test.fasta", "w")
-with open("Cronobacter_OACs.fasta") as f:
-	for line in f:
-		if ">" in line and "galF" in line:
-			m = line.strip("\n").split("_")[1]
-			wf.write(">"+m+"\n")
-			wf.write(next(f))
+parser=argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='input the original file')
+parser.add_argument('-o', '--out', help='the out file')
+args=parser.parse_args()
 
-wf.close()
+def find_galF(i):
+	pt=os.getcwd()
+
+	if args.out:
+		wf=open(os.path.join(pt,args.out),'w')
+	else:
+		wf=open(os.path.join(pt,today),'w')
+
+	with open(os.path.join(pt,args.input)) as f:
+		for line in f:
+			if "galF" in line:
+				wf.write(line)
+				wf.write(next(f))
+	wf.close()
+
+
+#if __name__=='__main__':
+#	find_galF(args.input)
+
+
